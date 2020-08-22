a3415d21f6775f479d620e6c2c052d1e29ba86a4
Amanda_Katherine <walker.akw@gmail.com>
2020-08-22 00:06:22 -0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,21 +6,50 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
-    erb :'/pets/new'
+    @owners = Owner.all
+    erb :"/pets/new"
   end
 
   post '/pets' do 
+    @pet = Pet.create(params[:pet])
+
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+
+    @pet.save
 
     redirect to "pets/#{@pet.id}"
   end
 
+  
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
+  
+  get '/pets/:id/edit' do 
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
 
   patch '/pets/:id' do 
+    # binding.pry
+    ####### allows user to remove all previous pets from the owner. 
+    if !params[:pet].keys.include?("owner_id")
+    params[:pet]["owner_id"] = []
+    end
+    #######
+  
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
 
+    @pet.save
+    
     redirect to "pets/#{@pet.id}"
   end
 end
